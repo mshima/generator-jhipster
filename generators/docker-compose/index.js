6d2567faaca34295f2b63a5b8c48c1482ef99ca3
Marcelo Shima <marceloshima@gmail.com>
2021-09-11 11:19:32 -0300

    Refactoring docker-compose

--- generators/docker-compose/index.js
+++ generators/docker-compose/index.js
@@ -137,141 +137,163 @@ module.exports = class extends BaseDockerGenerator {
 
   _preparing() {
     return {
+      checkAppsConfig() {
+        let portIndex = 8080;
+        this.serverPort = portIndex;
+        const serverPorts = this.appConfigs.map(({ serverPort }) => serverPort);
+        if (
+          serverPorts.some(serverPort => !serverPort) ||
+          !serverPorts.some(serverPort => serverPort === this.serverPort) ||
+          serverPorts.length !== new Set(serverPorts).size
+        ) {
+          // create new ports in case some port is missing, root port doesn't exists, or duplicated ports
+          this.appConfigs.forEach(appConfig => {
+            appConfig.serverPort = portIndex;
+            portIndex++;
+          });
+        }
+      },
+
       loadConfig() {
         this.usesOauth2 = this.appConfigs.some(appConfig => appConfig.authenticationTypeOauth2);
-        this.useKafka = this.appConfigs.some(appConfig => appConfig.messageBroker === KAFKA);
+        this.useKafka = this.appConfigs.some(({ messageBroker }) => messageBroker === KAFKA);
+        this.useMemcached = this.appConfigs.some(({ cacheProvider }) => cacheProvider === MEMCACHED);
+        this.authenticationType = this.appConfigs.find(({ authenticationType }) => authenticationType).authenticationType;
+
+        this.keycloakRedirectUris = this.appConfigs
+          .map(({ serverPort, devServerPort }) => {
+            const keycloakRedirectUris = [`"http://localhost:${serverPort}/*", "https://localhost:${serverPort}/*"`];
+            if (devServerPort !== undefined) {
+              keycloakRedirectUris.push(`"http://localhost:${devServerPort}/*, "https://localhost:${devServerPort}/*""`);
+            }
+            return keycloakRedirectUris;
+          })
+          .flat()
+          .join(', ');
       },
 
       setAppsYaml() {
-        this.appsYaml = [];
-        this.keycloakRedirectUris = '';
-        let portIndex = 8080;
-        this.serverPort = portIndex;
-        this.appConfigs.forEach(appConfig => {
-          const lowercaseBaseName = appConfig.baseName.toLowerCase();
-          const parentConfiguration = {};
-          const path = this.destinationPath(this.directoryPath + appConfig.appFolder);
-          // Add application configuration
-          const yaml = jsyaml.load(this.fs.read(`${path}/src/main/docker/app.yml`));
-          const yamlConfig = yaml.services[`${lowercaseBaseName}-app`];
-          if (appConfig.applicationType === GATEWAY || appConfig.applicationType === MONOLITH) {
-            this.keycloakRedirectUris += `"http://localhost:${portIndex}/*", "https://localhost:${portIndex}/*", `;
-            if (appConfig.devServerPort !== undefined) {
-              this.keycloakRedirectUris += `"http://localhost:${appConfig.devServerPort}/*", `;
-            }
-            // Split ports by ":" and take last 2 elements to skip the hostname/IP if present
-            const ports = yamlConfig.ports[0].split(':').slice(-2);
-            ports[0] = portIndex;
-            yamlConfig.ports[0] = ports.join(':');
-            portIndex++;
-          }
-
-          if (appConfig.applicationType === MONOLITH && this.monitoring === PROMETHEUS) {
-            yamlConfig.environment.push('JHIPSTER_LOGGING_LOGSTASH_ENABLED=false');
-            yamlConfig.environment.push('MANAGEMENT_METRICS_EXPORT_PROMETHEUS_ENABLED=true');
-          }
-
-          if (this.serviceDiscoveryType === EUREKA) {
-            // Set the JHipster Registry password
-            yamlConfig.environment.push(`JHIPSTER_REGISTRY_PASSWORD=${this.adminPassword}`);
-          }
-
-          if (!this.serviceDiscoveryType && appConfig.skipClient) {
-            yamlConfig.environment.push('SERVER_PORT=80'); // to simplify service resolution in docker/k8s
-          }
-
-          parentConfiguration[`${lowercaseBaseName}`] = yamlConfig;
-
-          // Add database configuration
-          const database = appConfig.prodDatabaseType;
-          if (database !== NO_DATABASE && database !== ORACLE) {
-            const relativePath = pathjs.relative(this.destinationRoot(), `${path}/src/main/docker`);
-            const databaseYaml = jsyaml.load(this.fs.read(`${path}/src/main/docker/${database}.yml`));
-            const databaseServiceName = `${lowercaseBaseName}-${database}`;
-            let databaseYamlConfig = databaseYaml.services[databaseServiceName];
-            if (database !== MARIADB) delete databaseYamlConfig.ports;
-
-            if (database === CASSANDRA) {
-              // node config
-              const cassandraClusterYaml = jsyaml.load(this.fs.read(`${path}/src/main/docker/cassandra-cluster.yml`));
-              const cassandraNodeConfig = cassandraClusterYaml.services[`${databaseServiceName}-node`];
-              parentConfiguration[`${databaseServiceName}-node`] = cassandraNodeConfig;
-
-              // migration service config
-              const cassandraMigrationYaml = jsyaml.load(this.fs.read(`${path}/src/main/docker/cassandra-migration.yml`));
-              const cassandraMigrationConfig = cassandraMigrationYaml.services[`${databaseServiceName}-migration`];
-              cassandraMigrationConfig.build.context = relativePath;
-              const createKeyspaceScript = cassandraClusterYaml.services[`${databaseServiceName}-migration`].environment[0];
-              cassandraMigrationConfig.environment.push(createKeyspaceScript);
-              const cqlFilesRelativePath = pathjs.relative(this.destinationRoot(), `${path}/src/main/resources/config/cql`);
-              cassandraMigrationConfig.volumes[0] = `${cqlFilesRelativePath}:/cql:ro`;
-
-              parentConfiguration[`${databaseServiceName}-migration`] = cassandraMigrationConfig;
-            }
-
-            if (database === COUCHBASE) {
-              databaseYamlConfig.build.context = relativePath;
-            }
-
-            if (appConfig.clusteredDb) {
-              const clusterDbYaml = jsyaml.load(this.fs.read(`${path}/src/main/docker/${database}-cluster.yml`));
-              const dbNodeConfig = clusterDbYaml.services[`${databaseServiceName}-node`];
-              dbNodeConfig.build.context = relativePath;
-              databaseYamlConfig = clusterDbYaml.services[databaseServiceName];
-              delete databaseYamlConfig.ports;
-              if (database === COUCHBASE) {
-                databaseYamlConfig.build.context = relativePath;
+        const services = this.appConfigs
+          .map(
+            ({
+              baseName,
+              applicationType,
+              serverPort,
+              skipClient,
+              prodDatabaseType: database,
+              clusteredDb,
+              searchEngine,
+              cacheProvider,
+              appFolder,
+              appRelativePath = this.directoryPath || './',
+              platformMonitoring = this.monitoring,
+              platformServiceDiscoveryType = this.serviceDiscoveryType,
+              appPath = this.destinationPath(appRelativePath + appFolder),
+            }) => {
+              const appServices = [];
+              const lowercaseBaseName = baseName.toLowerCase();
+              // Add application configuration
+              const yaml = jsyaml.load(this.fs.read(`${appPath}/src/main/docker/app.yml`));
+              const yamlConfig = yaml.services[`${lowercaseBaseName}-app`];
+              if (applicationType === GATEWAY || applicationType === MONOLITH) {
+                // Split ports by ":" and take last 2 elements to skip the hostname/IP if present
+                const ports = yamlConfig.ports[0].split(':').slice(-2);
+                ports[0] = serverPort;
+                yamlConfig.ports[0] = ports.join(':');
               }
-              parentConfiguration[`${databaseServiceName}-node`] = dbNodeConfig;
-              if (database === MONGODB) {
-                parentConfiguration[`${databaseServiceName}-config`] = clusterDbYaml.services[`${databaseServiceName}-config`];
+
+              if (applicationType === MONOLITH && platformMonitoring === PROMETHEUS) {
+                yamlConfig.environment.push('JHIPSTER_LOGGING_LOGSTASH_ENABLED=false');
+                yamlConfig.environment.push('MANAGEMENT_METRICS_EXPORT_PROMETHEUS_ENABLED=true');
               }
+
+              if (this.serviceDiscoveryType === EUREKA) {
+                // Set the JHipster Registry password
+                yamlConfig.environment.push(`JHIPSTER_REGISTRY_PASSWORD=${this.adminPassword}`);
+              }
+
+              if (!platformServiceDiscoveryType && skipClient) {
+                yamlConfig.environment.push('SERVER_PORT=80'); // to simplify service resolution in docker/k8s
+              }
+
+              appServices.push([`${lowercaseBaseName}`, yamlConfig]);
+
+              // Add database configuration
+              if (database !== NO_DATABASE && database !== ORACLE) {
+                const relativePath = pathjs.relative(this.destinationRoot(), `${appPath}/src/main/docker`);
+                const databaseYaml = jsyaml.load(this.fs.read(`${appPath}/src/main/docker/${database}.yml`));
+                const databaseServiceName = `${lowercaseBaseName}-${database}`;
+                let databaseYamlConfig = databaseYaml.services[databaseServiceName];
+                if (database !== MARIADB) delete databaseYamlConfig.ports;
+
+                if (database === CASSANDRA) {
+                  // node config
+                  const cassandraClusterYaml = jsyaml.load(this.fs.read(`${appPath}/src/main/docker/cassandra-cluster.yml`));
+                  const cassandraNodeConfig = cassandraClusterYaml.services[`${databaseServiceName}-node`];
+                  appServices.push([`${databaseServiceName}-node`, cassandraNodeConfig]);
+
+                  // migration service config
+                  const cassandraMigrationYaml = jsyaml.load(this.fs.read(`${appPath}/src/main/docker/cassandra-migration.yml`));
+                  const cassandraMigrationConfig = cassandraMigrationYaml.services[`${databaseServiceName}-migration`];
+                  cassandraMigrationConfig.build.context = relativePath;
+                  const createKeyspaceScript = cassandraClusterYaml.services[`${databaseServiceName}-migration`].environment[0];
+                  cassandraMigrationConfig.environment.push(createKeyspaceScript);
+                  const cqlFilesRelativePath = pathjs.relative(this.destinationRoot(), `${appPath}/src/main/resources/config/cql`);
+                  cassandraMigrationConfig.volumes[0] = `${cqlFilesRelativePath}:/cql:ro`;
+
+                  appServices.push([`${databaseServiceName}-migration`, cassandraMigrationConfig]);
+                }
+
+                if (database === COUCHBASE) {
+                  databaseYamlConfig.build.context = relativePath;
+                }
+
+                if (clusteredDb) {
+                  const clusterDbYaml = jsyaml.load(this.fs.read(`${appPath}/src/main/docker/${database}-cluster.yml`));
+                  const dbNodeConfig = clusterDbYaml.services[`${databaseServiceName}-node`];
+                  dbNodeConfig.build.context = relativePath;
+                  databaseYamlConfig = clusterDbYaml.services[databaseServiceName];
+                  delete databaseYamlConfig.ports;
+                  if (database === COUCHBASE) {
+                    databaseYamlConfig.build.context = relativePath;
+                  }
+                  appServices.push([`${databaseServiceName}-node`, dbNodeConfig]);
+                  if (database === MONGODB) {
+                    appServices.push([`${databaseServiceName}-config`, clusterDbYaml.services[`${databaseServiceName}-config`]]);
+                  }
+                }
+
+                appServices.push([databaseServiceName, databaseYamlConfig]);
+              }
+              // Add search engine configuration
+              if (searchEngine === ELASTICSEARCH) {
+                const searchEngineYaml = jsyaml.load(this.fs.read(`${appPath}/src/main/docker/${searchEngine}.yml`));
+                const searchEngineConfig = searchEngineYaml.services[`${lowercaseBaseName}-${searchEngine}`];
+                delete searchEngineConfig.ports;
+                appServices.push([`${lowercaseBaseName}-${searchEngine}`, searchEngineConfig]);
+              }
+
+              // Add Memcached support
+              if (cacheProvider === MEMCACHED) {
+                const memcachedYaml = jsyaml.load(this.fs.read(`${appPath}/src/main/docker/memcached.yml`));
+                const memcachedConfig = memcachedYaml.services[`${lowercaseBaseName}-memcached`];
+                delete memcachedConfig.ports;
+                appServices.push([`${lowercaseBaseName}-memcached`, memcachedConfig]);
+              }
+
+              // Add Redis support
+              if (cacheProvider === REDIS) {
+                const redisYaml = jsyaml.load(this.fs.read(`${appPath}/src/main/docker/redis.yml`));
+                const redisConfig = redisYaml.services[`${lowercaseBaseName}-redis`];
+                delete redisConfig.ports;
+                appServices.push([`${lowercaseBaseName}-redis`, redisConfig]);
+              }
+              return appServices;
             }
-
-            parentConfiguration[databaseServiceName] = databaseYamlConfig;
-          }
-          // Add search engine configuration
-          const searchEngine = appConfig.searchEngine;
-          if (searchEngine === ELASTICSEARCH) {
-            const searchEngineYaml = jsyaml.load(this.fs.read(`${path}/src/main/docker/${searchEngine}.yml`));
-            const searchEngineConfig = searchEngineYaml.services[`${lowercaseBaseName}-${searchEngine}`];
-            delete searchEngineConfig.ports;
-            parentConfiguration[`${lowercaseBaseName}-${searchEngine}`] = searchEngineConfig;
-          }
-          // Add Memcached support
-          const cacheProvider = appConfig.cacheProvider;
-          if (cacheProvider === MEMCACHED) {
-            this.useMemcached = true;
-            const memcachedYaml = jsyaml.load(this.fs.read(`${path}/src/main/docker/memcached.yml`));
-            const memcachedConfig = memcachedYaml.services[`${lowercaseBaseName}-memcached`];
-            delete memcachedConfig.ports;
-            parentConfiguration[`${lowercaseBaseName}-memcached`] = memcachedConfig;
-          }
-
-          // Add Redis support
-          if (cacheProvider === REDIS) {
-            this.useRedis = true;
-            const redisYaml = jsyaml.load(this.fs.read(`${path}/src/main/docker/redis.yml`));
-            const redisConfig = redisYaml.services[`${lowercaseBaseName}-redis`];
-            delete redisConfig.ports;
-            parentConfiguration[`${lowercaseBaseName}-redis`] = redisConfig;
-          }
-          // Expose authenticationType
-          this.authenticationType = appConfig.authenticationType;
-
-          // Dump the file
-          let yamlString = jsyaml.dump(parentConfiguration, { indent: 2, lineWidth: -1 });
-
-          // Add extra indentation for each lines
-          const yamlArray = yamlString.split('\n');
-          for (let j = 0; j < yamlArray.length; j++) {
-            yamlArray[j] = `  ${yamlArray[j]}`;
-          }
-          yamlString = yamlArray.join('\n');
-          this.appsYaml.push(yamlString);
-
-          this.skipClient = appConfig.skipClient;
-        });
+          )
+          .flat();
+        // Dump the file
+        this.yamlFile = jsyaml.dump({ services: Object.fromEntries(services) }, { indent: 2, lineWidth: -1 });
       },
     };
   }
@@ -301,11 +323,9 @@ module.exports = class extends BaseDockerGenerator {
     this.log(`You can launch all your infrastructure by running : ${chalk.cyan('docker-compose up -d')}`);
     if (this.gatewayNb + this.monolithicNb > 1) {
       this.log('\nYour applications will be accessible on these URLs:');
-      let portIndex = 8080;
       this.appConfigs.forEach(appConfig => {
         if (appConfig.applicationType === GATEWAY || appConfig.applicationType === MONOLITH) {
-          this.log(`\t- ${appConfig.baseName}: http://localhost:${portIndex}`);
-          portIndex++;
+          this.log(`\t- ${appConfig.baseName}: http://localhost:${appConfig.serverPort}`);
         }
       });
       this.log('\n');
