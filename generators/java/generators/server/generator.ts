089c4fc8b2891de165023a48216ae366bf8059bb
Marcelo Shima <marceloshima@gmail.com>
2026-08-04 16:51:28 -0300

    adjusts

--- generators/java/generators/server/generator.ts
+++ generators/java/generators/server/generator.ts
@@ -77,7 +77,6 @@ export default class ServerGenerator extends JavaApplicationGenerator {
             'java:docker': './gradlew bootJar -Pprod jibDockerBuild',
             'java:docker:arm64': 'npm run java:docker -- -PjibArchitecture=arm64',
             'backend:unit:test': `./gradlew test integrationTest ${excludeWebapp} ${javaCommonLog} ${javaTestLog}`,
-            'postci:e2e:package': 'cp build/libs/*.$npm_package_config_packaging e2e.$npm_package_config_packaging',
             'backend:build-cache':
               'npm run backend:info && npm run backend:nohttp:test && npm run ci:e2e:package -- -x webapp -x webapp_test',
           });
