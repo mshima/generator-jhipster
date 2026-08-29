1c42d5829f0b028f277592a83c711643b749da66
Marcelo Shima <marceloshima@gmail.com>
2026-08-29 08:28:57 -0300

    Revert "angular: adjusts"
    This reverts commit 5db014f9de535a9f626cfebb6c31c863892842eb.

--- generators/angular/generator.ts
+++ generators/angular/generator.ts
@@ -459,7 +459,6 @@ export default class AngularGenerator extends AngularApplicationGenerator {
           },
           devDependencies: {
             '@angular-architects/native-federation': null,
-            '@angular-devkit/architect': null,
           },
         });
       },
